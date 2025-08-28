c787b716f3d8ced1dfa73566b7ab3bcfbe79a5b4
Bharani951 <bharani.22it@kct.ac.in>
2025-08-28 11:28:25 +0530

    configured full application

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from "./routes/notesRoutes.js"; // Importing the notes routes
 // const express = require("express"); // using CommonJS syntax
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
@@ -14,11 +15,15 @@ const app = express();
 
 const port = process.env.PORT || 5001;
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+const __dirname = path.resolve();
+
+if (proccess.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // frontend url
+    })
+  );
+}
 //middleware- before sending the response we need to send the request to the body
 app.use(express.json()); // this middleware parses incoming JSON requests and puts the parsed data in req.body
 // to send the message of what we are going to do with the API in the postman
@@ -31,6 +36,13 @@ app.use(express.json()); // this middleware parses incoming JSON requests and pu
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 // first connect to the DB first and then start connecting to the port
 connectDB().then(() => {
